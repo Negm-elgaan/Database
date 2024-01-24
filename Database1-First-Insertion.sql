a3999624822def191fa599e23698f50d1cd458fa
Negm-elgaan <ohossam962@gmai.com>
2024-01-24 05:37:51 +0200

    Insertion

---
+++ Database1-First-Insertion.sql
@@ -0,0 +1,18 @@
+INSERT INTO Access_to_Basic_Services
+(
+	Region,
+    Sub_region,
+    Country_name,
+    Time_period,
+    Pct_managed_drinking_water_services,
+    Pct_managed_sanitation_services,
+    Est_population_in_millions,
+    Est_gdp_in_billions,
+    Land_area,
+    Pct_unemployment
+)
+VALUES
+(
+'Sub-Saharan Africa','Southern Africa','South Africa',2020,92.0,78.67,58.801927,337.62,1213090.0,24.34
+);
+
