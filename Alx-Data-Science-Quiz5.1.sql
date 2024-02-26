35c86b13ca51008ce1d6719e4edb82a883a72c60
Negm-elgaan <ohossam962@gmai.com>
2024-02-26 04:31:54 +0200

    My answer is Correct :-)

---
+++ Alx-Data-Science-Quiz5.1.sql
@@ -0,0 +1,14 @@
+#Question4 answer
+USE md_water_services;
+#SELECT
+#	*
+#FROM
+#	data_dictionary
+#WHERE
+#	  column_name LIKE "p%";
+SELECT
+	*
+FROM
+	global_water_access
+WHERE
+	name = "Maji Ndogo";
